eece875e83a16ccda376ba3ca5e227c80a7f34c3
dvfoff <138278040+dvfoff@users.noreply.github.com>
2023-07-01 03:31:27 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+functon setup() {
+    createCanvas(600, 600);
+    background("black");
+}
+
+function draw() {
+    stroke("blue");
+    fill("red");
+
+    if(mouseIsPressed){
+        Reflect(mouseX, mouseY, 20, 35);
+    }
+    
+}
